716a470ceea391606a724856acee6107e343a117
devjk <rmsghk4254@naver.com>
2024-02-26 19:54:12 +0900

    feat(client): vuex 적용

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import store from './store/store.js'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.mount('#app')
 
 
 
